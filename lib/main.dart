813c86fa63500694c0467aea495f157e408cfeb5
dungbk <conandung9999@gmail.com>
2021-03-10 17:47:45 +0700

    [update] use static variables to avoid error typing

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
+import 'screens/chat_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+
 void main() => runApp(FlashChat());
 
 class FlashChat extends StatelessWidget {
@@ -15,12 +20,12 @@ class FlashChat extends StatelessWidget {
           bodyText2: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: 'welcome',
+      initialRoute: WelcomeScreen.id,
       routes: {
-        'welcome': (context) => WelcomeScreen(),
-        'login': (context) => LoginScreen(),
-        'register': (context) => RegistrationScreen(),
-        'chat': (context) => ChatScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
       // home: WelcomeScreen(),
     );
